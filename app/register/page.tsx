86d8a2b733aa8b0ed702e89d07976f9244402190
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2026-08-03 03:11:05 +0300

    remov slash

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -33,7 +33,7 @@ export default function RegisterPage() {
     setLoading(true)
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}users`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ phone, password }),
