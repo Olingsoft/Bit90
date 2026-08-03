4b44fa73c94f98213ab2cb434bc82e44bd7a1e20
olinga-abdulaziz <abdulazizolinga17@gmail.com>
2026-08-03 17:03:22 +0300

    trade page

--- app/trade/page.tsx
+++ app/trade/page.tsx
@@ -1,11 +1,16 @@
+'use client'
+
+import { useState } from "react";
 import Header from "@/components/Header";
 
 export default function Trade() {
-    return (
-        <div className="min-h-screen w-full bg-[#0B0E14] text-[#ECEEF3]">
-            <Header query="" setQuery={() => { }} />
-            <h1>Trade</h1>
-            <p className="text-center">comming soon</p>
-        </div>
-    )
+  const [query, setQuery] = useState("");
+
+  return (
+    <div className="min-h-screen w-full bg-[#0B0E14] text-[#ECEEF3]">
+      <Header query={query} setQuery={setQuery} />
+      <h1>Trade</h1>
+      <p className="text-center">coming soon</p>
+    </div>
+  );
 }
